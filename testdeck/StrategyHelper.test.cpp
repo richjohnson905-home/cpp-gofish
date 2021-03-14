b1def5bf46c2b3c0b6211a36bb5c4a8b20e93c4f
Rich Johnson <rich.johnson@monsanto.com>
2021-03-14 10:30:41 -0500

    update

--- testdeck/StrategyHelper.test.cpp
+++ testdeck/StrategyHelper.test.cpp
@@ -3,18 +3,19 @@
 #include "StrategyHelper.h"
 #include "MockPlayer.h"
 
-
 using namespace std;
 using ::testing::NiceMock;
 using ::testing::Return;
 using ::testing::_;
 using ::testing::SetArgReferee;
+using ::testing::DoAll;
 
 TEST(StrategyHelperTest, GoEasyFishing) {
+    Deck deck;
     StrategyHelper testObject;
-    NiceMock<MockPlayer> me;
-    NiceMock<MockPlayer> other1;
-    NiceMock<MockPlayer> other2;
+    NiceMock<MockPlayer> me("TestObj", deck);
+    NiceMock<MockPlayer> other1("Other1", deck);
+    NiceMock<MockPlayer> other2("Other2", deck);
     vector<Player*> otherPlayers = {&other1, &other2};
     vector<Card*> myCards = {
         new Card(3, Card::hearts),
@@ -25,33 +26,17 @@ TEST(StrategyHelperTest, GoEasyFishing) {
         new Card(3, Card::diamonds),
         new Card(7, Card::diamonds)
     };
-    ON_CALL(me, getHand()).WillByDefault(Return(myCards));
+    ON_CALL(me, getHand()).WillByDefault(Return(&myCards));
     EXPECT_CALL(other1, hasEasyFish(3)).WillOnce(Return(true));
-    EXPECT_CALL(other1, hasEasyFish(4)).WillOnce(Return(false));
     ON_CALL(other2, hasEasyFish(_)).WillByDefault(Return(false));
-    EXPECT_CALL(me, otherHasCards(&other1, _, _))
-        .WillOnce(SetArgReferee<1>(othersCards));
+    EXPECT_CALL(me, otherHasCards(&other1, _, 3))
+        .WillOnce(DoAll(SetArgReferee<1>(othersCards), Return(true)));
 
     vector<Card*> actual = testObject.goEasyFishing(&me, otherPlayers, bait);
+
+    EXPECT_EQ(2, actual.size());
 }
-/*
-TEST_F(StrategyTest, getFishableCards) {
-    Deck d;
-    StrategyHelper testObject(m_me, d);
+
+TEST(StrategyHelperTest, FOO) {
     
-    vector<Card*> myCards = {
-        new Card(2, Card::hearts), 
-        new Card(2, Card::hearts),
-        new Card(3, Card::hearts),
-        new Card(4, Card::hearts),
-        new Card(5, Card::hearts),
-        new Card(6, Card::hearts),
-        new Card(8, Card::hearts)
-        };
-    ON_CALL(m_me, getHand()).WillByDefault(Return(&myCards));
-
-    set<Card*, CardCompare> fishableCards = testObject.getPossibleBaitCards();
-
-    EXPECT_EQ(6, fishableCards.size());
 }
-*/
