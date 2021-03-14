021f89fbed141e818433ca43cb434258425a307c
Rich Johnson <rich.johnson@monsanto.com>
2021-03-14 17:19:07 -0500

    add vscode and new test.

--- testdeck/StrategyHelper.test.cpp
+++ testdeck/StrategyHelper.test.cpp
@@ -37,6 +37,38 @@ TEST(StrategyHelperTest, GoEasyFishing) {
     EXPECT_EQ(2, actual.size());
 }
 
-TEST(StrategyHelperTest, FOO) {
-    
+TEST(StrategyHelperTest, goFishing_goFish) {
+    Deck deck;
+    StrategyHelper testObject;
+    NiceMock<MockPlayer> me("TestObj", deck);
+    NiceMock<MockPlayer> other1("Other1", deck);
+    vector<Player*> otherPlayers = {&other1};
+    int bait = 3;
+
+    EXPECT_CALL(me, otherHasCards(&other1, _, bait))
+        .WillOnce(Return(false));
+
+    vector<Card*> actual = testObject.goFishing(&me, deck, otherPlayers, bait);
+
+    EXPECT_EQ(1, actual.size());
+
+}
+
+TEST(StrategyHelperTest, getBaitCard) {
+    Deck deck;
+    StrategyHelper testObject;
+    NiceMock<MockPlayer> me("TestObj", deck);
+    vector<Card*> myCards = {
+        new Card(3, Card::hearts),
+        new Card(3, Card::diamonds),
+        new Card(4, Card::diamonds),
+        new Card(4, Card::clubs)
+    };
+
+    EXPECT_CALL(me, getHand()).WillRepeatedly(Return(&myCards));
+
+    int bait = testObject.getBaitCard(&me);
+
+    EXPECT_TRUE(bait == 3 || bait == 4);
+
 }
